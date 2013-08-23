5b8637fdf975a6da61dd8c9421e3eb8eca154763
mario martinez <amakenadog@gmail.com>
2013-08-23 09:08:04 -0600

    added new properties to domain class Department

--- grails-app/domain/ni/edu/uccleon/Department.groovy
+++ grails-app/domain/ni/edu/uccleon/Department.groovy
@@ -3,12 +3,16 @@ package ni.edu.uccleon
 class Department {
 
     String name
+    String manager
+    String extention
 
 	Date dateCreated
 	Date lastUpdated
 
     static constraints = {
         name blank:false, unique:true
+        manager blank:false
+        extention blank:false
     }
 
     static mapping = {
